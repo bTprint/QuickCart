e4dffe5c5a27424bae57f7a47af300aa503ce67f
bTprint <50615097+bTprint@users.noreply.github.com>
2025-03-27 22:20:17 +0100

    small fix

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,7 +2,7 @@ import { serve } from "inngest/next";
 import { Inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
-    client : inngest,
+    client : Inngest,
     functions : [
         syncUserCreation,
         syncUserUpdation,
