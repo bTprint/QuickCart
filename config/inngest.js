7b77dd3d456ff65c03eea4d0006c8382348361a3
bTprint <50615097+bTprint@users.noreply.github.com>
2025-03-28 09:26:19 +0100

    small fix v2.1.1

--- config/inngest.js
+++ config/inngest.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/modals/User";
 
-export const inngest = new Inngest({ id: "tomboleromania"});
+export const inngest = new Inngest({ id: "tomboleromania" });
 
 // Functia Inngest pentru a salva un user in baza de date
 export const syncUserCreation = inngest.createFunction(
@@ -10,13 +10,13 @@ export const syncUserCreation = inngest.createFunction(
         id: 'sync-user-from-clerk'
     },
     { event: 'clerk/user.created' },
-    async ({event})=> {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url: image_url
+            imageUrl: image_url
         }
         await connectDB()
         await User.create(userData)
@@ -29,13 +29,13 @@ export const syncUserUpdation = inngest.createFunction(
         id: 'update-user-from-clerk'
     },
     { event: 'clerk/user.updated' },
-    async ({event})=> {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url: image_url
+            imageUrl: image_url
         }
         await connectDB()
         await User.findByIdAndUpdate(id,userData)
@@ -47,10 +47,8 @@ export const syncUserDeletion = inngest.createFunction(
     {
         id: 'delete-user-with-clerk'
     },
-    {
-        event: 'clerk/user.deleted'
-    },
-    async ({event})=> {
+    { event: 'clerk/user.deleted' },
+    async ({event}) => {
         const { id } = event.data
 
         await connectDB()
